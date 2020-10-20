ce3b1b5831005773322a3ff18ff1f550229f5819
darya <kokorina.darya1509@gmail.com>
2020-10-20 15:15:19 +0300

    remove akitaNgDevtools to app module

--- apps/employees-system/src/app/app.module.ts
+++ apps/employees-system/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 
 import { AppComponent } from './app.component';
 import { FakeBackendInterceptor } from './fake-backend';
@@ -8,7 +9,7 @@ import { FeatureMarshmallowContainerModule } from '@marshmallow-land/feature-mar
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FeatureMarshmallowContainerModule, HttpClientModule],
+  imports: [BrowserModule, FeatureMarshmallowContainerModule, HttpClientModule, AkitaNgDevtools.forRoot()],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: FakeBackendInterceptor, multi: true}
   ],
